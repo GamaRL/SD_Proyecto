3c026ddab687174ccd7913d864f2cf519f85b883
Alexis Sánchez <xtheggx@gmail.com>
2024-05-26 00:52:22 -0600

    ADD: Comentarios de server, heartbeat y tokenpass

--- src/main/java/mx/unam/fi/distributed/messages/settings/HeartBeat.java
+++ src/main/java/mx/unam/fi/distributed/messages/settings/HeartBeat.java
@@ -22,6 +22,9 @@ public class HeartBeat implements Runnable {
     @Value("${app.server.node_n}")
     private int nodeN;
 
+    /**
+     * Envía un mensaje multicast a todos los nodos conocidos.
+     */
     private void sendMulticastMessage() {
         nodeRepository.getNodes().forEach((n) -> client.sendMessage(n, new Message(nodeN, "HELLO", LocalDateTime.now())));
     }
@@ -34,15 +37,19 @@ public class HeartBeat implements Runnable {
 
         while (isAlive) {
             try {
+                // Pausa la ejecución durante 2 segundos
                 Thread.sleep(2000);
 
+                // Envía un mensaje multicast a todos los nodos
                 sendMulticastMessage();
 
+                // Determina el nodo maestro basándose en el ID más alto
                 int masterId = nodeRepository.getNodesId().stream().mapToInt(i -> i).max().orElse(-1);
 
                 if (masterId == nodeN) {
                     System.out.println("> Yo soy el master");
-
+                  
+                    //Verifica si era el maestro anteriormente
                     if (!isMaster) {
                         var newToken = String.format("TOKEN;%d", nodeN);
                         System.out.println("Sending the message...");
--- src/main/java/mx/unam/fi/distributed/messages/settings/TokenPass.java
+++ src/main/java/mx/unam/fi/distributed/messages/settings/TokenPass.java
@@ -13,29 +13,44 @@ public class TokenPass implements Runnable {
 
     private final Semaphore lock;
 
+    /**
+     * Método de inicialización que se ejecuta después de la construcción del objeto.
+     * Inicia un nuevo hilo que ejecuta el método run() de esta clase.
+     */
     @PostConstruct
-    void init()
+    void init() 
     {
         new Thread(this).start();
     }
 
+    /**
+     * Método principal que se ejecuta en el hilo.
+     * Realiza un ciclo infinito donde intenta adquirir el semáforo, imprime un mensaje,
+     * y luego libera el semáforo.
+     */
     @Override
     public void run() {
-
+        
         while (true) {
             try {
+                // Adquiere el semáforo para asegurar la exclusión mutua
                 lock.acquire();
             } catch (InterruptedException e) {
+                // Maneja la excepción de interrupción del hilo
                 System.out.println(e.getMessage());
             }
 
+            // Imprime un mensaje indicando que tiene el token
             System.out.println("I have the token");
 
+            // Libera el semáforo
             lock.release();
 
             try {
+                // Pausa la ejecución del hilo por 500 milisegundos
                 Thread.sleep(500);
             } catch (InterruptedException e) {
+                // Maneja la excepción de interrupción del hilo y lanza una RuntimeException
                 throw new RuntimeException(e);
             }
         }
