7ba4bea5c878069e9554ee67cb448c15262eff22
Aldo Santiago Alejandro <26aldo.santiago@gmail.com>
2024-05-28 01:15:28 -0600

    Add: Comentarios a Client y Listeners

--- src/main/java/mx/unam/fi/distributed/messages/listeners/MessageEvent.java
+++ src/main/java/mx/unam/fi/distributed/messages/listeners/MessageEvent.java
@@ -5,11 +5,20 @@ import org.springframework.context.ApplicationEvent;
 
 import java.net.Socket;
 
+/**
+ * Representa un evento de los mensajes que se reciben a través de una conexión de socket
+ */
 @Getter
 public class MessageEvent extends ApplicationEvent {
 
     private final Socket socket;
 
+    /**
+     * Constructor de un nuevo MessageEvent
+     * 
+     * @param source Objeto en el que ocurre inicialmente el evento
+     * @param socket Socket a traves del cual se recibió el mensaje
+     */
     public MessageEvent(Object source, Socket socket) {
         super(source);
         this.socket = socket;
--- src/main/java/mx/unam/fi/distributed/messages/listeners/NoReachableNodeEvent.java
+++ src/main/java/mx/unam/fi/distributed/messages/listeners/NoReachableNodeEvent.java
@@ -4,6 +4,12 @@ import lombok.Getter;
 import mx.unam.fi.distributed.messages.node.Node;
 import org.springframework.context.ApplicationEvent;
 
+
+/**
+ * Es usada cuando se produce una excepcion de nodo inalcanzable.
+ * Un nodo se vuelve inalcanzable cuando se pasa el tiempo de espera o algun
+ * error en el socket.
+ */
 @Getter
 public class NoReachableNodeEvent extends ApplicationEvent {
     private final Node node;
