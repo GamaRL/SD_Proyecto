2b8d81609821eec065e7c5650a7cfe5249029606
Aldo Santiago Alejandro <26aldo.santiago@gmail.com>
2024-05-28 01:43:06 -0600

    Add: Comentarios a Nodo.java y repositories

--- src/main/java/mx/unam/fi/distributed/messages/repositories/DeviceRepository.java
+++ src/main/java/mx/unam/fi/distributed/messages/repositories/DeviceRepository.java
@@ -4,5 +4,8 @@ import mx.unam.fi.distributed.messages.models.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Interfaz de repositorio para la entidad Device.
+ */
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {}
--- src/main/java/mx/unam/fi/distributed/messages/repositories/EngineerRepository.java
+++ src/main/java/mx/unam/fi/distributed/messages/repositories/EngineerRepository.java
@@ -4,5 +4,8 @@ import mx.unam.fi.distributed.messages.models.Engineer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Interfaz de repositorio para la entidad Engineer.
+ */
 @Repository
 public interface EngineerRepository extends JpaRepository<Engineer, Long> {}
